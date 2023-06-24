75b301e0da0156488aef04e20a22bf58c2b9d9cb
br00klyn93 <bmclaury93@gmail.com>
2023-06-24 21:06:08 +0200

    Update plsfix.py

--- plsfix.py
+++ plsfix.py
@@ -1,11 +1,22 @@
 from flask import Flask, request, render_template, redirect
 from flask import make_response, Response
+import json
+
 app = Flask(__name__)
+
+f = open('prompts.json')
+p = json.load(f)
 
 @app.route('/')
 def main():
     print("All working!")
     return render_template('index.html')
+
+@app.route('/getFix')
+def getFix():
+    print(p['prompts'][-1])
+    return(p['prompts'][-1])
+    
     
 
 if __name__ == "__main__":
