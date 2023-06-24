d966107a8849e94b4dcc6aa767107b45c42a6a3c
br00klyn93 <bmclaury93@gmail.com>
2023-06-24 20:37:27 +0200

    Update plsfix.py

--- plsfix.py
+++ plsfix.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     print("All working!")
-    return render_template('index.html')
+    
 
 if __name__ == "__main__":
     app.run("0.0.0.0",port=5000)
